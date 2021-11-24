84e0bbe7cdb1b63e03e91b41266e31f3068fc07d
elvisthermo <elvisthermo@gmail.com>
2021-11-24 19:18:19 -0300

    Adição de itens no carrinho
    - descrição por arquivos
    - ordem-compra.component.htm: add adição e remoção de itens e calculos
    
    pedido.model.ts:
    - add novo campo para compra

--- src/app/carrinho.service.ts
+++ src/app/carrinho.service.ts
@@ -1,10 +1,15 @@
-import { ItemCarrinho } from './shared/item-carrinho.model';
 import { Oferta } from './shared/oferta.model';
+import { ItemCarrinho } from './shared/item-carrinho.model';
+import { ThisReceiver } from '@angular/compiler';
 
 export class CarrinhoService {
   public itens: ItemCarrinho[] = [];
 
-  incluirItem(oferta: Oferta) {
+  public exibirItens(): ItemCarrinho[] {
+    return this.itens;
+  }
+
+  public incluirItem(oferta: Oferta): void {
     let itemCarrinho: ItemCarrinho = new ItemCarrinho(
       oferta.id,
       oferta.imagens[0],
@@ -13,10 +18,52 @@ export class CarrinhoService {
       oferta.valor,
       1
     );
-    this.itens.push(itemCarrinho);
+    //verificar se o item em questao ja nao existe dentro de this.itens
+    let itemCarrinhoEncontrado = this.itens.find(
+      (item: ItemCarrinho) => item.id === itemCarrinho.id
+    );
+    if (itemCarrinhoEncontrado) {
+      itemCarrinhoEncontrado.quantidade += 1;
+    } else {
+      this.itens.push(itemCarrinho);
+    }
   }
 
-  public exibirItens(): ItemCarrinho[] {
-    return this.itens;
+  public totalCarrinhoCompras(): number {
+    let total: number = 0;
+    this.itens.map((item: ItemCarrinho) => {
+      total = total + item.valor * item.quantidade;
+    });
+    return total;
+  }
+
+  public adicionarQuantidade(itemCarrinho: ItemCarrinho): void {
+    //incrementar a quantidade
+    let itemCarrinhoEncontrado = this.itens.find(
+      (item: ItemCarrinho) => item.id === itemCarrinho.id
+    );
+
+    if (itemCarrinhoEncontrado) {
+      itemCarrinhoEncontrado.quantidade += 1;
+    }
+  }
+
+  public diminuirQuantidade(itemCarrinho: ItemCarrinho): void {
+    //decrementar quantidade
+    let itemCarrinhoEncontrado = this.itens.find(
+      (item: ItemCarrinho) => item.id === itemCarrinho.id
+    );
+
+    if (itemCarrinhoEncontrado) {
+      itemCarrinhoEncontrado.quantidade -= 1;
+
+      if(itemCarrinhoEncontrado.quantidade === 0) {
+         this.itens.splice(this.itens.indexOf(itemCarrinhoEncontrado), 1)
+      }
+    }
+  }
+
+  public limparCarrinho(): void {
+    this.itens = []
   }
 }
--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -2,42 +2,50 @@ import { Observable, of, Subject } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
-import { catchError, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import {
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  switchMap,
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
   styleUrls: ['./topo.component.css'],
-  providers: [ OfertasService ]
+  providers: [OfertasService],
 })
 export class TopoComponent implements OnInit {
+  public ofertas!: Observable<Oferta[]>;
+  private subjectPesquisa: Subject<string> = new Subject<string>();
 
-  public ofertas!: Observable<Oferta[]>
-  private subjectPesquisa: Subject<string> = new Subject<string>()
-
-  constructor(private ofertasService: OfertasService) { }
+  constructor(private ofertasService: OfertasService) {}
 
   ngOnInit(): void {
-     this.ofertas = this.subjectPesquisa //retorno ao termino Oferta[]
-        .pipe(debounceTime(1000))  //executa a acao do switchMap apos 1 segundo
-        .pipe(distinctUntilChanged())  //para fazer pesquisas distintas
-        .pipe(switchMap((termo: string) => {
-            if(termo.trim() === '') {
-              //retornar um observable de array de ofertas vazio
-              return of<Oferta[]>([])
-            }
-            return this.ofertasService.pesquisarOfertas(termo)
-        }))
-        .pipe(catchError((err: any) => {
-          return of<Oferta[]>([])
-        }))
+    this.ofertas = this.subjectPesquisa //retorno ao termino Oferta[]
+      .pipe(debounceTime(1000)) //executa a acao do switchMap apos 1 segundo
+      .pipe(distinctUntilChanged()) //para fazer pesquisas distintas
+      .pipe(
+        switchMap((termo: string) => {
+          if (termo.trim() === '') {
+            //retornar um observable de array de ofertas vazio
+            return of<Oferta[]>([]);
+          }
+          return this.ofertasService.pesquisarOfertas(termo);
+        })
+      )
+      .pipe(
+        catchError((err: any) => {
+          return of<Oferta[]>([]);
+        })
+      );
   }
 
   public pesquisa(termoDaBusca: string): void {
-      this.subjectPesquisa.next(termoDaBusca)
+    this.subjectPesquisa.next(termoDaBusca);
   }
 
   public limpaPesquisa(): void {
-    this.subjectPesquisa.next('')
+    this.subjectPesquisa.next('');
   }
 }
